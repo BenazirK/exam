9caa835880ff215d707c9dd3063d63f132eed6c9
BenazirK <kaaryevab@gmail.com>
2020-04-20 18:00:12 +0600

    right

--- app.py
+++ app.py
@@ -2,10 +2,24 @@ from flask import Flask , render_template, request
 
 app= Flask(__name__)
 
-@app.route("/", methods=["GET" ,"POST"])
-def gallery():
-    return render_template("gallery.html")
+@app.route("/")
+def index():
+    f = open("urls.txt", "r", encoding="utf-8")
+    u = [row for row in f]
+    f.close()
+    return render_template("index.html", urls=u)
+
 
 @app.route("/add")
 def add():
+
     return render_template("add.html")
+
+@app.route("/reciever" , methods=["POST"])
+def reciever():
+    url = request.form.get("url")    
+    f = open("urls.txt", "a+", encoding="utf-8")
+    f.write("\n" + url)
+    sources = [row for row in f if row]    
+    f.close()
+    return render_template("index.html", urls=sources)
